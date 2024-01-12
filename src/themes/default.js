51b05fbae85edd383b1b927d555f120358a8ee1b
AkhilllYadav <akhilyadawa@gmail.com>
2024-01-12 23:37:32 +0530

    First commit

---
+++ src/themes/default.js
@@ -0,0 +1,26 @@
+export default {
+    // Temp fonts
+    fonts: {
+      title: "Space Grotesk, sans-serif",
+      main: "Space Grotesk, sans-serif"
+    },
+    // Colors for layout
+    colors: {
+      primary1: "#be1adb",
+      background1: "#f0f0f0",
+      button: "#5c5b5b",
+      background2: "#FFFFFF",
+      text: "#111111",
+      text1: "#48494a",
+      text2: "#626970",
+      text3: "#575C66",
+      footerBackground: "#FFFFFF"
+    },
+    // Breakpoints for responsive design
+    breakpoints: {
+      sm: 'screen and (max-width: 640px)',
+      md: 'screen and (max-width: 768px)',
+      lg: 'screen and (max-width: 1024px)',
+      xl: 'screen and (max-width: 1280px)'
+    },
+  }
